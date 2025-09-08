60a378f253644417283856a046550d6b3fc786e2
misterO <mistero4321@gmail.com>
2025-09-09 00:08:25 +0200

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import type React from "react"
+import "@/app/globals.css"
+import { Inter } from "next/font/google"
+import { ThemeProvider } from "@/components/theme-provider"
+import Navbar from "@/components/navbar"
+
+const inter = Inter({ subsets: ["latin"] })
+
+export const metadata = {
+  title: "Frontend Developer | Olivier KOUASSI",
+  description: "Frontend Developer specializing in Next & React & tailwindCSS",
+  icons: {
+    icon: [{ url: "/images/kihuni.png" }],
+  },
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+          <Navbar />
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  )
+}
